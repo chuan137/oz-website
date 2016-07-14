c305739441b558cde7afc499c2cda62f59fcdd1f
Chuan Miao <chuan137@gmail.com>
2016-07-14 12:14:29 +0200

    Init 'Hello World'

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const NpmInstallPlugin = require('npm-install-webpack-plugin');
+
+const PATHS = {
+  app: path.join(__dirname, './app'),
+  build: path.resolve(__dirname, './build')
+};
+
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        include: PATHS.app,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.jsx?$/,
+        include: PATHS.app,
+        loaders: ['babel?cacheDirectory']
+      }
+    ]
+  }
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({ save: true })
+    ]
+  })
+}
+
+if (TARGET === 'build') {
+  module.exports = wp_merge(common, {});
+}
