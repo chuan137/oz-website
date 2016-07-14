fe3a01478962f5635e5e455183e779a5e05cff24
Chuan Miao <chuan137@gmail.com>
2016-07-14 16:49:19 +0200

    NEW react component for multiple-step-form

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ const CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
 
 const PATHS = {
   app: path.join(__dirname, './app'),
-  build: path.resolve(__dirname, './client')
+  build: path.resolve(__dirname, './client'),
+  lib: path.resolve(__dirname, './app/lib')
 };
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -20,6 +21,11 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      lib: PATHS.lib
+    }
   },
   module: {
     loaders: [
